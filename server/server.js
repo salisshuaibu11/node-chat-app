92f76502b0363eb0fae65afdeec275d0a7878e39
Salisu <salisshuaibu11@gmail.com>
2021-04-25 00:59:50 -0400

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,22 @@ app.use(express.static(publicPath));
 io.on("connection", (socket) => {
   console.log("New user connected");
 
+  socket.emit("newMessage", {
+    from: "john",
+    text: "See you then",
+    createdAt: 123123,
+  });
+
+  socket.emit("newEmail", {
+    from: "salis@gmail.com",
+    text: "Hey, what is going on",
+    createdAt: 123,
+  });
+
+  socket.on("createMessage", (message) => {
+    console.log("createMessage", message);
+  });
+
   socket.on("disconnect", () => {
     console.log("User disconnected ");
   });
